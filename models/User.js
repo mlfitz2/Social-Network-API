400c45c83c5bd172f2ec905b4b69f3509e740134
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-05-20 20:59:13 -0700

    set up thoughts model

--- models/User.js
+++ models/User.js
@@ -39,13 +39,10 @@ const userSchema = new Schema(
   }
 );
 
-// get total count of friends on retrieval
 userSchema.virtual("friendCount").get(function () {
   return this.friends.length;
 });
 
-// create the user model using userSchema
 const User = model("User", userSchema);
 
-// export the model
 module.exports = User;
