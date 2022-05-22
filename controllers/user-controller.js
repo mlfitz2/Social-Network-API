bd5404e4e3436f496e98e461bca224f4c94cb4c9
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-05-21 22:38:52 -0700

    bug fixes in user controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,34 +1,38 @@
-const { User } = require("../models");
-const { populate } = require("../models/User");
+const res = require('express/lib/response');
+const { User } = require('../models');
+
 
 const userController = {
   getAllUsers(req, res) {
     User.find({})
       .populate({
-        path: "thoughts",
-        select: "-__v",
+        path: 'thoughts',
+        select: '-__v',
       })
-      .select("-__v")
-      .sort({ _id: -1 })
-      .then((dbUserData) => res.json(dbUserData))
-      .catch((err) => {
-        console.log(err);
-        res.status(400).json(err);
-      });
+      .populate({
+        path: 'friends',
+        select: '-__v',
+      })
+      .select('-__v')
+      .then((results) => res.json(results))
+      .catch((err) => res.status(500).json(err))
   },
   getUserById({ params }, res) {
     User.findOne({ _id: params.id })
       .populate({
-        path: "thoughts",
-        select: "-__v",
+        path: 'thoughts',
+        select: '-__v',
       })
-      .select("-__v")
-      .then((dbUserData) => {
-        if (!dbUserData) {
-          res.status(404).json({ message: "No user found with this ID" });
+      .select('-__v')
+      .then((results) => {
+        if (!results) {
+          res.status(404).json({
+            message:
+              'No user with that ID found, try again with different ID please.',
+          });
           return;
         }
-        res.json(dbUserData);
+        res.json(results);
       })
       .catch((err) => {
         console.log(err);
@@ -37,54 +41,58 @@ const userController = {
   },
   createUser({ body }, res) {
     User.create(body)
-      .then((dbUserData) => res.json(dbUserData))
+      .then((results) => res.json(results))
       .catch((err) => res.status(400).json(err));
   },
   updateUser({ params, body }, res) {
     User.findOneAndUpdate({ _id: params.id }, body, { new: true })
-      .then((dbUserData) => {
-        if (!dbUserData) {
-          res.status(404).json({ message: "No user found with this ID" });
+      .then((results) => {
+        if (!results) {
+          res
+            .status(404)
+            .json({ message: 'No user with that ID found, try again!' });
           return;
         }
-        res.json(dbUserData);
+        res.json(results);
       })
       .catch((err) => res.status(400).json(err));
   },
   deleteUser({ params }, res) {
     User.findOneAndDelete({ _id: params.id })
-      .then((dbUserData) => {
-        if (!dbUserData) {
-          res.status(404).json({ message: "no user found with this ID" });
+      .then((results) => {
+        if (!results) {
+          res
+            .status(404)
+            .json({ message: 'No user with that ID found, try again' });
           return;
         }
-        res.json(dbUserData);
+        res.json(results);
       })
       .catch((err) => res.status(400).json(err));
   },
-
   addFriend({ params }, res) {
     User.findOneAndUpdate(
       { _id: params.id },
       { $addToSet: { friends: params.friendsId } },
       { new: true }
     )
-      .then((dbUserData) => res.json(dbUserData))
+      .then((results) => res.status(200).json(results))
       .catch((err) => res.status(400).json(err));
   },
-
-  removeFriend({ params }, res) {
+  deleteFriend({ params }, res) {
     User.findOneAndUpdate(
       { _id: params.id },
       { $pull: { friends: params.friendsId } },
       { new: true }
     )
-      .then((dbUserData) => {
-        if (!dbUserData) {
-          res.status(404).json({ message: "no user found with this ID" });
+      .then((results) => {
+        if (!results) {
+          res
+            .status(404)
+            .json({ message: 'No results found with that ID, try again' });
           return;
         }
-        res.json(dbUserData);
+        res.json(results);
       })
       .catch((err) => res.status(400).json(err));
   },
